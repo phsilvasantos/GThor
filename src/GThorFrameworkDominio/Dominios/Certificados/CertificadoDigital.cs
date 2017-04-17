5ab67b66eca6ab3b0bbaa026d032719587e2107f
robertorp <robertoalvespereira18@gmail.com>
2017-04-16 21:55:19 -0300

    Adicionado Dominio CertificadoDigital

---
+++ src/GThorFrameworkDominio/Dominios/Certificados/CertificadoDigital.cs
@@ -0,0 +1,10 @@
+﻿namespace GThorFrameworkDominio.Dominios.Certificados
+{
+    public class CertificadoDigital
+    {
+        public int Id { get; set; }
+        public string CaminhoCertificado { get; set; }
+        public string Serial { get; set; }
+        public TipoCertificado Tipo { get; set; }
+    }
+}
---
+++ src/GThorFrameworkDominio/Dominios/Certificados/TipoCertificado.cs
@@ -0,0 +1,9 @@
+﻿namespace GThorFrameworkDominio.Dominios.Certificados
+{
+    public enum TipoCertificado
+    {
+        A1Arquivo = 1,
+        A1Repositorio = 2,
+        A3 = 3
+    }
+}
