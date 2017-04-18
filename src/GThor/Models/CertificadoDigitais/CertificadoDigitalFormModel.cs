faec4462c0e794d4af944ffe6242b8dd167209d3
robertorp <robertoalvespereira18@gmail.com>
2017-04-17 21:40:24 -0300

    Criado Model para CertificadoDigitalForm

---
+++ src/GThor/Models/CertificadoDigitais/CertificadoDigitalFormModel.cs
@@ -0,0 +1,57 @@
+﻿using GThorFrameworkDominio.Dominios.Certificados;
+using GThorFrameworkWpf.Models.Base;
+
+namespace GThor.Models.CertificadoDigitais
+{
+    public class CertificadoDigitalFormModel : ModelViewBase
+    {
+        private TipoCertificado _tipoCertificado;
+        private string _arquivo;
+        private string _serial;
+        private string _senha;
+
+        public TipoCertificado TipoCertificado
+        {
+            get => _tipoCertificado;
+            set
+            {
+                if (value == _tipoCertificado) return;
+                _tipoCertificado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Arquivo
+        {
+            get => _arquivo;
+            set
+            {
+                if (value == _arquivo) return;
+                _arquivo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Serial
+        {
+            get => _serial;
+            set
+            {
+                if (value == _serial) return;
+                _serial = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Senha
+        {
+            get => _senha;
+            set
+            {
+                if (value == _senha) return;
+                _senha = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
