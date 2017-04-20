ebfeb005b95f6340d6e9d2415bb414d65167652a
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-20 00:22:37 -0300

    documentoMdfe adicionado

---
+++ src/GThorFrameworkDominio/Dominios/DocumentosFiscaisEletronicos/DocumentoMDFe.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos.flags;
+
+namespace GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos
+{
+    [Table("documentoMdfe")]
+    public class DocumentoMdfe
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("ambienteSefaz")]
+        public AmbienteSefaz AmbienteSefaz { get; set; }
+
+        [Required]
+        [Column("serie")]
+        public short Serie { get; set; }
+
+        [Required]
+        [Column("ultimoNumeroUsado")]
+        public long UltimoNumeroUsado { get; set; }
+    }
+}
---
+++ src/GThorFrameworkDominio/Dominios/DocumentosFiscaisEletronicos/flags/AmbienteSefaz.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using GThorFrameworkWpf.Helpers.Conversores;
+
+namespace GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos.flags
+{
+    [TypeConverter(typeof(EnumTypeDescriptionConverter))]
+    public enum AmbienteSefaz
+    {
+        [Description("Produção")]
+        Producao = 1,
+
+        [Description("Homologação")]
+        Homologacao = 2
+    }
+}
