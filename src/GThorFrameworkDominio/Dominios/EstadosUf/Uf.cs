3a170ea42c426e411ba4ce4d4a5908a85def8ce7
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-19 21:52:47 -0300

    Adicionado objeto uf (unidade federativa)

---
+++ src/GThorFrameworkDominio/Dominios/EstadosUf/Uf.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GThorFrameworkDominio.Dominios.EstadosUf
+{
+    [Table("uf")]
+    public class Uf
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("sigla")]
+        [Required]
+        [MaxLength(2)]
+        public string Sigla { get; set; }
+
+        [Column("nome")]
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [Column("codigoIbge")]
+        [Required]
+        public byte CodigoIbge { get; set; }
+    }
+}
