e0a59c8710ce2854a6ec67936f29212b5121b398
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-20 19:34:13 -0300

    Feito NegocioDocumentoMdfe

---
+++ src/GThorNegocio/Contratos/INegocioDocumentoMdfe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos;
+
+namespace GThorNegocio.Contratos
+{
+    public interface INegocioDocumentoMdfe
+    {
+        void SalvarOuAtualizar(DocumentoMdfe documentoMdfe);
+        IEnumerable<DocumentoMdfe> Lista();
+        void Deletar(DocumentoMdfe documentoMdfe);
+    }
+}
---
+++ src/GThorNegocio/Negocios/NegocioDocumentoMdfe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos;
+using GThorNegocio.Contratos;
+using GThorRepositorio.Contratos;
+
+namespace GThorNegocio.Negocios
+{
+    public class NegocioDocumentoMdfe : INegocioDocumentoMdfe
+    {
+        private readonly IRepositorioDocumentoMdfe _repositorioDocumentoMdfe;
+
+        public NegocioDocumentoMdfe(IRepositorioDocumentoMdfe repositorioDocumentoMdfe)
+        {
+            _repositorioDocumentoMdfe = repositorioDocumentoMdfe;
+        }
+
+
+        public void SalvarOuAtualizar(DocumentoMdfe documentoMdfe)
+        {
+            _repositorioDocumentoMdfe.SalvarOuAtualizar(documentoMdfe);
+        }
+
+        public IEnumerable<DocumentoMdfe> Lista()
+        {
+            return _repositorioDocumentoMdfe.Lista();
+        }
+
+        public void Deletar(DocumentoMdfe documentoMdfe)
+        {
+            _repositorioDocumentoMdfe.Deletar(documentoMdfe);
+        }
+    }
+}
