7fb97b9e5f4143d8cfff88bf3a1cc0761092c46f
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-20 22:22:17 -0300

    Feito NegocioVeiculo

---
+++ src/GThorNegocio/Contratos/INegocioVeiculo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using GThorFrameworkDominio.Dominios.Veiculos;
+
+namespace GThorNegocio.Contratos
+{
+    public interface INegocioVeiculo
+    {
+        void SalvarOuAtualizar(Veiculo veiculo);
+        IEnumerable<Veiculo> Lista();
+        void Deletar(Veiculo veiculo);
+    }
+}
---
+++ src/GThorNegocio/Negocios/NegocioVeiculo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using GThorFrameworkDominio.Dominios.Veiculos;
+using GThorNegocio.Contratos;
+using GThorRepositorio.Contratos;
+using GThorRepositorioEntityFramework.Contexto;
+using GThorRepositorioEntityFramework.Extensoes;
+
+namespace GThorNegocio.Negocios
+{
+    public class NegocioVeiculo : INegocioVeiculo
+    {
+        private readonly IRepositorioVeiculo _repositorioVeiculo;
+
+        public NegocioVeiculo(IRepositorioVeiculo repositorioVeiculo)
+        {
+            _repositorioVeiculo = repositorioVeiculo;
+        }
+
+        public void SalvarOuAtualizar(Veiculo veiculo)
+        {
+            using (var contexto = new GThorContexto())
+            {
+                _repositorioVeiculo.SetGThorContexto(contexto);
+                _repositorioVeiculo.SalvarOuAtualizar(veiculo);
+                _repositorioVeiculo.SalvarAlteracoes();
+            }
+        }
+
+        public IEnumerable<Veiculo> Lista()
+        {
+            using (var contexto = new GThorContexto())
+            {
+                _repositorioVeiculo.SetGThorContexto(contexto);
+                return _repositorioVeiculo.Lista();
+            }
+        }
+
+        public void Deletar(Veiculo veiculo)
+        {
+            using (var contexto = new GThorContexto())
+            {
+                _repositorioVeiculo.SetGThorContexto(contexto);
+                _repositorioVeiculo.Deletar(veiculo);
+                _repositorioVeiculo.SalvarAlteracoes();
+            }
+        }
+    }
+}
