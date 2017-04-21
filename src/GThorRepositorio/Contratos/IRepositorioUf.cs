4b48d43741ffefb9b404043feba6ac4324a41a61
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-21 16:03:05 -0300

    Criado RepositorioUf

---
+++ src/GThorRepositorio/Contratos/IRepositorioUf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GThorFrameworkDominio.Dominios.EstadosUf;
+using GThorFrameworkRepositorio.Contratos;
+
+namespace GThorRepositorio.Contratos
+{
+    public interface IRepositorioUf :
+        IDaoContexto,
+        IDaoBase<Uf, int>,
+        IDisposable
+    {
+        
+    }
+}
---
+++ src/GThorRepositorioEntityFramework/Implementacao/RepositorioUf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GThorFrameworkDominio.Dominios.EstadosUf;
+using GThorRepositorio.Contratos;
+using GThorRepositorioEntityFramework.Implementacao.Base;
+
+namespace GThorRepositorioEntityFramework.Implementacao
+{
+    public class RepositorioUf : RepositorioBase, IRepositorioUf
+    {
+        public Uf CarregarPorId(int id)
+        {
+            return GThorContexto.Ufs.FirstOrDefault(uf => uf.Id == id);
+        }
+
+        public IEnumerable<Uf> Lista()
+        {
+            return GThorContexto.Ufs.ToList();
+        }
+    }
+}
