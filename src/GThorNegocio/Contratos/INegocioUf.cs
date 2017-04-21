0c091e602feb2590e7b52a7acab0c4eb5f05cccb
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-21 16:05:34 -0300

    Criado NegocioUf

---
+++ src/GThorNegocio/Contratos/INegocioUf.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using GThorFrameworkDominio.Dominios.EstadosUf;
+
+namespace GThorNegocio.Contratos
+{
+    public interface INegocioUf
+    {
+        IEnumerable<Uf> Lista();
+    }
+}
---
+++ src/GThorNegocio/Negocios/NegocioUf.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using GThorFrameworkDominio.Dominios.EstadosUf;
+using GThorNegocio.Contratos;
+using GThorRepositorio.Contratos;
+using GThorRepositorioEntityFramework.Contexto;
+using GThorRepositorioEntityFramework.Extensoes;
+
+namespace GThorNegocio.Negocios
+{
+    public class NegocioUf : INegocioUf
+    {
+        private readonly IRepositorioUf _repositorioUf;
+
+        public NegocioUf(IRepositorioUf repositorioUf)
+        {
+            _repositorioUf = repositorioUf;
+        }
+
+        public IEnumerable<Uf> Lista()
+        {
+            using (var contexto = new GThorContexto())
+            {
+                _repositorioUf.SetGThorContexto(contexto);
+                return _repositorioUf.Lista();
+            }
+        }
+    }
+}
