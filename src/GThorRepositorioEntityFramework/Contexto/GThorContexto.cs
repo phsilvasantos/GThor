bdf3faddd801cad035813a0ccc738f0b778901f0
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-23 01:28:29 -0300

    Adicionado Log padrão para entity framework

--- src/GThorRepositorioEntityFramework/Contexto/GThorContexto.cs
+++ src/GThorRepositorioEntityFramework/Contexto/GThorContexto.cs
@@ -4,6 +4,7 @@ using GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos;
 using GThorFrameworkDominio.Dominios.EstadosUf;
 using GThorFrameworkDominio.Dominios.Usuarios;
 using GThorFrameworkDominio.Dominios.Veiculos;
+using GThorRepositorioEntityFramework.Log;
 using Microsoft.EntityFrameworkCore;
 
 namespace GThorRepositorioEntityFramework.Contexto
@@ -13,6 +14,11 @@ namespace GThorRepositorioEntityFramework.Contexto
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Database=gthor;Username=postgres;Password=root");
+
+        #if DEBUG
+            optionsBuilder.UseLoggerFactory(new EfLoggerFactory());
+        #endif
+
         }
 
         public DbSet<Usuario> Usuarios { get; set; }
