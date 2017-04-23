1e2527e79e0fde595cc7e0202daf2f5463777936
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-23 01:34:08 -0300

    Adicionado criado de contexto

---
+++ src/GThorRepositorioEntityFramework/Criadores/ContextoCriador.cs
@@ -0,0 +1,13 @@
+﻿using GThorRepositorioEntityFramework.Contexto;
+using Microsoft.EntityFrameworkCore;
+
+namespace GThorRepositorioEntityFramework.Criadores
+{
+    public class ContextoCriador
+    {
+        public DbContext CriaContexto()
+        {
+            return new GThorContexto();
+        }
+    }
+}
