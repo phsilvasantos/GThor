658bacfc079f549f6b2904c3cc9e36d28c5d40ad
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-26 20:36:20 -0300

    Adiconando migrations

---
+++ src/GThorMigracaoBancoDados/Migracoes/CriaSchema.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace GThorMigracaoBancoDados.Migracoes
+{
+    [Migration(1)]
+    public class CriaSchema : Migration
+    {
+        public override void Up()
+        {
+            IfDatabase("teste").Create.Schema("teste");
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
