ffbfe27290d6950ab4941a81a912e8a4481b0470
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-26 18:57:53 -0300

    adicionado configuracao via código do nhibernate

--- src/GThorRepositorioNHibernate/Criadores/CriaSessionFactoryPostgreSql.cs
+++ src/GThorRepositorioNHibernate/Criadores/CriaSessionFactoryPostgreSql.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using GThorRepositorioNHibernate.Criadores.Contratos;
 using NHibernate;
 using NHibernate.Cfg;
@@ -13,11 +14,32 @@ namespace GThorRepositorioNHibernate.Criadores
 
             var assembly = Assembly.GetAssembly(GetType());
 
-            cfg.Configure(assembly, "GThorRepositorioNHibernate.hibernate.cfg.xml");
-
+            cfg.AddProperties(ObterPropriedades());
+            cfg.AddAssembly(assembly);
             var iSessionFactory = cfg.BuildSessionFactory();
 
             return iSessionFactory;
+        }
+
+        private IDictionary<string, string> ObterPropriedades()
+        {
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var property = new Dictionary<string, string>();
+
+            property.Add("connection.provider", "NHibernate.Connection.DriverConnectionProvider");
+            property.Add("connection.isolation", "ReadUncommitted");
+            property.Add("dialect", "NHibernate.Dialect.PostgreSQL82Dialect");
+            property.Add("connection.driver_class", "NHibernate.Driver.NpgsqlDriver");
+            property.Add("connection.connection_string", "Server=localhost;Port=5432;User ID=postgres;Password=root;Database=gthor");
+            property.Add("cache.use_second_level_cache", "false");
+
+#if DEBUG
+            property.Add("show_sql", "true");
+            property.Add("format_sql", "true");
+            property.Add("adonet.batch_size", "0");
+#endif
+
+            return property;
         }
     }
 }
