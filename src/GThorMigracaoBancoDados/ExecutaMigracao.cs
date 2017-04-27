5bbcfd47744ac5b6bb84889a5afadfafa7883e96
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-26 21:48:00 -0300

    adicionado runners

---
+++ src/GThorMigracaoBancoDados/ExecutaMigracao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+
+namespace GThorMigracaoBancoDados
+{
+    public class ExecutaMigracao
+    {
+        public void Executa()
+        {
+            IAnnouncer announcer = new TextWriterAnnouncer(Console.Out);
+
+            IRunnerContext migrationContext = new RunnerContext(announcer)
+            {
+                Connection = "Server=localhost;Port=5432;User ID=postgres;Password=root;",
+                Database = "gthor"
+            };
+
+            TaskExecutor executor = new TaskExecutor(migrationContext);
+
+            executor.Execute();
+        }
+    }
+}
