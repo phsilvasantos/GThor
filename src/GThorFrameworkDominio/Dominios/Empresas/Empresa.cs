120f1fd04b8681de92ed42181fd13f5c96a2cbc6
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-26 22:36:12 -0300

    criado tabela empresa

--- src/GThorFrameworkDominio/Dominios/Empresas/Empresa.cs
+++ src/GThorFrameworkDominio/Dominios/Empresas/Empresa.cs
@@ -5,82 +5,38 @@ using GThorFrameworkDominio.Dominios.EstadosUf;
 
 namespace GThorFrameworkDominio.Dominios.Empresas
 {
-    [Table("empresa")]
     public class Empresa
     {
-        [Key]
-        [Column("id")]
-        [Required]
         public int Id { get; set; }
 
-        [Column("cnpj")]
-        [MaxLength(14)]
-        [Required]
         public string Cnpj { get; set; }
 
-        [Column("inscricaoEstadual")]
-        [MaxLength(30)]
-        [Required]
         public string InscricaoEstadual { get; set; }
 
-        [Column("rntrc")]
-        [MaxLength(8)]
-        [Required]
         public string Rntrc { get; set; }
 
-        [Column("razaoSocial")]
-        [MaxLength(255)]
-        [Required]
         public string RazaoSocial { get; set; }
 
-        [Column("nomeFantasia")]
-        [MaxLength(255)]
-        [Required]
         public string NomeFantasia { get; set; }
 
-        [Column("logradouro")]
-        [MaxLength(255)]
-        [Required]
         public string Logradouro { get; set; }
 
-        [Column("numero")]
-        [MaxLength(60)]
-        [Required]
         public string Numero { get; set; }
 
-        [Column("complemento")]
-        [MaxLength(60)]
-        [Required]
         public string Complemento { get; set; }
 
-        [Column("bairro")]
-        [MaxLength(60)]
-        [Required]
         public string Bairro { get; set; }
 
-        [Column("cidadeId")]
-        [Required]
         public int CidadeId { get; set; }
         public Cidade Cidade { get; set; }
 
-        [Column("cep")]
-        [MaxLength(8)]
-        [Required]
         public string Cep { get; set; }
 
-        [Column("ufId")]
-        [Required]
         public int UfId { get; set; }
         public Uf Uf { get; set; }
 
-        [Column("telefone")]
-        [MaxLength(11)]
-        [Required]
         public string Telefone { get; set; }
 
-        [Column("email")]
-        [MaxLength(255)]
-        [Required]
         public string Email { get; set; }
     }
 }
---
+++ src/GThorMigracaoBancoDados/Migracoes/CriaTabelaEmpresa.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+
+namespace GThorMigracaoBancoDados.Migracoes
+{
+    [Migration(7)]
+    public class CriaTabelaEmpresa : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("empresa")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("cnpj").AsString(14).NotNullable()
+                .WithColumn("inscricaoEstadual").AsString(30).NotNullable()
+                .WithColumn("rntrc").AsString(8).NotNullable()
+                .WithColumn("razaoSocial").AsString(255).NotNullable()
+                .WithColumn("nomeFantasia").AsString(255).NotNullable()
+                .WithColumn("logradouro").AsString(255).NotNullable()
+                .WithColumn("numero").AsString(60).NotNullable()
+                .WithColumn("complemento").AsString(60).NotNullable()
+                .WithColumn("bairro").AsString(60).NotNullable()
+                .WithColumn("cep").AsString(8).NotNullable()
+                .WithColumn("telefone").AsString(11).NotNullable()
+                .WithColumn("email").AsString(255).NotNullable()
+                .WithColumn("cidadeId").AsInt32().NotNullable()
+                .WithColumn("ufId").AsInt32().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("empresa");
+        }
+    }
+}
