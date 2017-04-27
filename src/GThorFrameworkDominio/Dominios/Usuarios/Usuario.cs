9522ef408c435174a905ed01be082925ce5e8987
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-26 21:40:26 -0300

    Tabela de usuario fluent migration

--- src/GThorFrameworkDominio/Dominios/Usuarios/Usuario.cs
+++ src/GThorFrameworkDominio/Dominios/Usuarios/Usuario.cs
@@ -1,25 +1,11 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace GThorFrameworkDominio.Dominios.Usuarios
+﻿namespace GThorFrameworkDominio.Dominios.Usuarios
 {
-    [Table("usuario")]
     public class Usuario
     {
-        [Key]
-        [Column("id")]
-        [Required]
         public virtual int Id { get; set; }
 
-        [Column("login")]
-        [MaxLength(20)]
-        [Required]
         public virtual string Login { get; set; }
 
-        [Column("senha")]
-        [MaxLength(40)]
-        [Required]
         public virtual string Senha { get; set; }
     }
 }
---
+++ src/GThorMigracaoBancoDados/Migracoes/CriaTabelaUsuario.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace GThorMigracaoBancoDados.Migracoes
+{
+    [Migration(1)]
+    public class CriaTabelaUsuario : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("usuario")
+                .WithColumn("id")
+                .AsInt32()
+                .PrimaryKey()
+                .Identity()
+                .WithColumn("login")
+                .AsString(20)
+                .NotNullable()
+                .WithColumn("senha")
+                .AsString(40)
+                .NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("usuario");
+        }
+    }
+}
