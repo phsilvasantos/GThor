c2638feea7833a69c7420e9758b764ff41726c83
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-26 22:30:10 -0300

    criado migração estadouf

--- src/GThorFrameworkDominio/Dominios/EstadosUf/Uf.cs
+++ src/GThorFrameworkDominio/Dominios/EstadosUf/Uf.cs
@@ -1,28 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GThorFrameworkDominio.Dominios.EstadosUf
 {
     [Table("uf")]
     public class Uf
     {
-        [Key]
-        [Column("id")]
-        [Required]
         public int Id { get; set; }
 
-        [Column("sigla")]
-        [Required]
-        [MaxLength(2)]
         public string Sigla { get; set; }
 
-        [Column("nome")]
-        [Required]
-        [MaxLength(100)]
         public string Nome { get; set; }
 
-        [Column("codigoIbge")]
-        [Required]
         public byte CodigoIbge { get; set; }
     }
 }
---
+++ src/GThorMigracaoBancoDados/Migracoes/CriaTabelaEstadoUf.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace GThorMigracaoBancoDados.Migracoes
+{
+    [Migration(6)]
+    public class CriaTabelaEstadoUf : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("uf")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("sigla").AsString(2).NotNullable()
+                .WithColumn("nome").AsString(100).NotNullable()
+                .WithColumn("codigoIbge").AsInt16().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("uf");
+        }
+    }
+}
