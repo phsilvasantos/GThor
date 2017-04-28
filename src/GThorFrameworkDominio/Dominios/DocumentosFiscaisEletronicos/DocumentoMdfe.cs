61f2142133a0a9192e91cfc864e21d27f668e6d3
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-04-27 23:21:11 -0300

    Adicionado acessores virtual pois o nhibernate esta no modo lazy=true por padrão.

--- src/GThorFrameworkDominio/Dominios/DocumentosFiscaisEletronicos/DocumentoMdfe.cs
+++ src/GThorFrameworkDominio/Dominios/DocumentosFiscaisEletronicos/DocumentoMdfe.cs
@@ -1,19 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos.flags;
+﻿using GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos.flags;
 
 namespace GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos
 {
     public class DocumentoMdfe
     {
-        public int Id { get; set; }
+        public virtual int Id { get; set; }
 
-        public string Descricao { get; set; }
+        public virtual string Descricao { get; set; }
 
-        public AmbienteSefaz AmbienteSefaz { get; set; } = AmbienteSefaz.Producao;
+        public virtual AmbienteSefaz AmbienteSefaz { get; set; } = AmbienteSefaz.Producao;
 
-        public short Serie { get; set; }
+        public virtual short Serie { get; set; }
 
-        public long UltimoNumeroUsado { get; set; }
+        public virtual long UltimoNumeroUsado { get; set; }
     }
 }
