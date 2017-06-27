6b421dafe86230bdadf4706650e24ee4a2250c28
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-06-27 20:00:55 -0300

    Simplificado perfilmdfe

--- src/GThorFrameworkDominio/Dominios/MdfeFiscal/Perfils/PerfilMdfe.cs
+++ src/GThorFrameworkDominio/Dominios/MdfeFiscal/Perfils/PerfilMdfe.cs
@@ -1,8 +1,6 @@
 ﻿using GThorFrameworkDominio.Dominios.Certificados;
 using GThorFrameworkDominio.Dominios.DocumentosFiscaisEletronicos;
 using GThorFrameworkDominio.Dominios.Empresas;
-using GThorFrameworkDominio.Dominios.EstadosUf;
-using GThorFrameworkDominio.Dominios.Veiculos;
 
 namespace GThorFrameworkDominio.Dominios.MdfeFiscal.Perfils
 {
@@ -13,10 +11,6 @@ namespace GThorFrameworkDominio.Dominios.MdfeFiscal.Perfils
         public virtual Empresa Empresa { get; set; }
         public virtual DocumentoMdfe DocumentoMdfe  { get; set; }
         public virtual CertificadoDigital CertificadoDigital { get; set; }
-        public virtual Uf UfCarregamento { get; set; }
-        public virtual Uf UfDescarregamento { get; set; }
-        public virtual Veiculo VeiculoTracao { get; set; }
-        public virtual string Observacao { get; set; }
 
     }
 }
--- src/GThorMigracaoBancoDados/Migracoes/CriaTabelaPerfilMdfe.cs
+++ src/GThorMigracaoBancoDados/Migracoes/CriaTabelaPerfilMdfe.cs
@@ -12,11 +12,7 @@ namespace GThorMigracaoBancoDados.Migracoes
                 .WithColumn("descricao").AsString(120).NotNullable()
                 .WithColumn("empresaId").AsInt32().NotNullable()
                 .WithColumn("documentoMdfeId").AsInt32().NotNullable()
-                .WithColumn("certificadoDigitalId").AsInt32().NotNullable()
-                .WithColumn("ufCarregamentoId").AsInt32().Nullable()
-                .WithColumn("ufDescarregamentoId").AsInt32().Nullable()
-                .WithColumn("veiculoTracaoId").AsInt32().Nullable()
-                .WithColumn("observacao").AsString(5000).NotNullable();
+                .WithColumn("certificadoDigitalId").AsInt32().NotNullable();
 
             Create.ForeignKey("fk_perfilMdfe__empresa")
                 .FromTable("perfilMdfe").ForeignColumn("empresaId")
@@ -29,18 +25,6 @@ namespace GThorMigracaoBancoDados.Migracoes
             Create.ForeignKey("fk_perfilMdfe__certificadoDigital")
                 .FromTable("perfilMdfe").ForeignColumn("certificadoDigitalId")
                 .ToTable("certificadoDigital").PrimaryColumn("id");
-
-            Create.ForeignKey("fk_perfilMdfe__ufCarregamento")
-                .FromTable("perfilMdfe").ForeignColumn("ufCarregamentoId")
-                .ToTable("uf").PrimaryColumn("id");
-
-            Create.ForeignKey("fk_perfilMdfe__ufDescarregamento")
-                .FromTable("perfilMdfe").ForeignColumn("ufDescarregamentoId")
-                .ToTable("uf").PrimaryColumn("id");
-
-            Create.ForeignKey("fk_perfilMdfe__veiculoTracao")
-                .FromTable("perfilMdfe").ForeignColumn("veiculoTracaoId")
-                .ToTable("veiculo").PrimaryColumn("id");
         }
 
         public override void Down()
