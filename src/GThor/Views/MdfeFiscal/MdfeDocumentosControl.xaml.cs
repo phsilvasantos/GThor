19e9c9185d8c9feee4456623a05e4ca825caadb0
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-07-07 01:29:02 -0300

    Ajustes MdfeCabecalho Fazendo MdfeDocumentosControl

---
+++ src/GThor/Views/MdfeFiscal/MdfeDocumentosControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GThor.Views.MdfeFiscal
+{
+    public partial class MdfeDocumentosControl
+    {
+        public MdfeDocumentosControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDoubleClickItem(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void OnClickDeletaItem(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
