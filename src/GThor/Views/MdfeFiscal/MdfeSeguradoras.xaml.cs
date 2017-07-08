9b0a52d7e52bb2c47ff88920f19ed0b6eb8a4f15
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-07-08 01:29:57 -0300

    Adicionado seguradora

---
+++ src/GThor/Views/MdfeFiscal/MdfeSeguradoras.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GThor.Views.MdfeFiscal
+{
+    /// <summary>
+    /// Interaction logic for MdfeSeguradora.xaml
+    /// </summary>
+    public partial class MdfeSeguradoras : UserControl
+    {
+        public MdfeSeguradoras()
+        {
+            InitializeComponent();
+        }
+    }
+}
