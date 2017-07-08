fa769d7d8efca33fd3d593ef19e2a89db2f1cf24
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-07-08 01:25:42 -0300

    Adicionado MdfeModalRodoviarioVeiculo

---
+++ src/GThor/Views/MdfeFiscal/MdfeModalRodoviarioVeiculo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GThor.Views.MdfeFiscal
+{
+    /// <summary>
+    /// Interaction logic for MdfeModalRodoviarioVeiculo.xaml
+    /// </summary>
+    public partial class MdfeModalRodoviarioVeiculo : UserControl
+    {
+        public MdfeModalRodoviarioVeiculo()
+        {
+            InitializeComponent();
+        }
+    }
+}
